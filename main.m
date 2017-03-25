feabe1f8bda34aff1af8bf5c32b4f1fe9602f113
xwli <buptlorraine@163.com>
2017-03-25 17:28:16 +0800

    give number to car and lower speed

--- main.m
+++ main.m
@@ -5,14 +5,32 @@ global D;
 global E;
 global F;
 global G;
+global G1;
+global G2;
+global G3;
+global G4;
+global G5;
+global G6;
+global G7;
+global G8;
+global G9;
 global X;
 global ffid;
-B=imread('C:\Users\Xwli\Desktop\warehouse.bmp');
-C=imread('C:\Users\Xwli\Desktop\xcrossing.bmp');
-D=imread('C:\Users\Xwli\Desktop\ycrossing.bmp');
-E=imread('C:\Users\Xwli\Desktop\right.bmp');
-F=imread('C:\Users\Xwli\Desktop\left.bmp');
-G=imread('C:\Users\Xwli\Desktop\car.bmp');
+B=imread('C:\Users\Xwli\Desktop\picture\warehouse.bmp');
+C=imread('C:\Users\Xwli\Desktop\picture\xcrossing.bmp');
+D=imread('C:\Users\Xwli\Desktop\picture\ycrossing.bmp');
+E=imread('C:\Users\Xwli\Desktop\picture\right.bmp');
+F=imread('C:\Users\Xwli\Desktop\picture\left.bmp');
+G=imread('C:\Users\Xwli\Desktop\picture\car.bmp');
+G1=imread('C:\Users\Xwli\Desktop\picture\car1.bmp');
+G2=imread('C:\Users\Xwli\Desktop\picture\car2.bmp');
+G3=imread('C:\Users\Xwli\Desktop\picture\car3.bmp');
+G4=imread('C:\Users\Xwli\Desktop\picture\car4.bmp');
+G5=imread('C:\Users\Xwli\Desktop\picture\car5.bmp');
+G6=imread('C:\Users\Xwli\Desktop\picture\car6.bmp');
+G7=imread('C:\Users\Xwli\Desktop\picture\car7.bmp');
+G8=imread('C:\Users\Xwli\Desktop\picture\car8.bmp');
+G9=imread('C:\Users\Xwli\Desktop\picture\car9.bmp');
 ffid = fopen('E:\code\proj\output.txt','r');
 tline = fgetl(ffid);
 tmp=tline;
@@ -29,7 +47,7 @@ for i=1:time+1
     end
     tline=fgetl(ffid);
     X=imaging(row,col);
-    name = ['C:\Users\Xwli\Desktop\result', num2str(time), '.bmp'];
+    name = ['C:\Users\Xwli\Desktop\picture\result', num2str(i), '.bmp'];
     imwrite(uint8(X),name);
 end
     
