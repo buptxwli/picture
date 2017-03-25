fadb3bc8867ca8e50eac296feb6cbe5ab6b5f45b
xwli <buptlorraine@163.com>
2017-03-25 17:28:35 +0800

    lower speed

--- vedio.m
+++ vedio.m
@@ -1,15 +1,16 @@
-function [ ] = video(  )
+function [ ] = video( time )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
-WriterObj2=VideoWriter('test2.avi');
+WriterObj2=VideoWriter('test3.avi');
 WriterObj2.FrameRate = 30;
 open(WriterObj2);
-for i=1:16
-    for j=1:20
-        name = ['C:\Users\Xwli\Desktop\result', num2str(i), '.bmp'];
+for i=1:time+1
+    for j=1:40
+        name = ['C:\Users\Xwli\Desktop\picture\result', num2str(i), '.bmp'];
         fame=imread(name);
         writeVideo(WriterObj2,fame);
     end
 end
 close(WriterObj2);
 end
+
